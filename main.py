17f3f18acad3bc974353905db0d0c247b10c2643
songhaibei <songhaibei7305@163.com>
2022-03-16 21:37:03 +0800

    数据集定义、加载和数据预处理
    框架自带数据集使用

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ paddle.__version__
 print('视觉相关数据集：', paddle.vision.datasets.__all__)
 print('自然语言相关数据集：', paddle.text.__all__)
 
+#%%
+
+from paddle.vision.transforms import ToTensor
+# 训练数据集
+train_dataset = vision.datasets.MNIST(mode='train', transform=ToTensor())
+
+# 验证数据集
+val_dataset = vision.datasets.MNIST(mode='test', transform=ToTensor())
+
