47f277fecc0407b66548a34c418aa8647580db8f
songhaibei <songhaibei7305@163.com>
2022-03-16 17:50:24 +0800

    用飞桨定义模型的计算

--- main.py
+++ main.py
@@ -16,3 +16,7 @@ print("paddle " + paddle.__version__)
 x_data = paddle.to_tensor([[1.], [3.0], [5.0], [9.0], [10.0], [20.0]])
 y_data = paddle.to_tensor([[12.], [16.0], [20.0], [28.0], [30.0], [50.0]])
 
+#%%
+
+linear = paddle.nn.Linear(in_features=1, out_features=1)
+
