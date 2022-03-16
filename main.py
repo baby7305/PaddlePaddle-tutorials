4b02c006f0a54b3912644ca465dc85014d062ef5
songhaibei <songhaibei7305@163.com>
2022-03-16 23:12:36 +0800

    模型部署
    模型存储

--- main.py
+++ main.py
@@ -356,3 +356,25 @@ result = model.evaluate(val_dataset, verbose=1)
 
 pred_result = model.predict(val_dataset)
 
+#%%
+
+class Mnist(paddle.nn.Layer):
+    def __init__(self):
+        super(Mnist, self).__init__()
+
+        self.flatten = paddle.nn.Flatten()
+        self.linear_1 = paddle.nn.Linear(784, 512)
+        self.linear_2 = paddle.nn.Linear(512, 10)
+        self.relu = paddle.nn.ReLU()
+        self.dropout = paddle.nn.Dropout(0.2)
+
+    @paddle.jit.to_static
+    def forward(self, inputs):
+        y = self.flatten(inputs)
+        y = self.linear_1(y)
+        y = self.relu(y)
+        y = self.dropout(y)
+        y = self.linear_2(y)
+
+        return y
+
