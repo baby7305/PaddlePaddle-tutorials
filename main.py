190abe695c7552cc05cbe218f7871594292a2557
songhaibei <songhaibei7305@163.com>
2022-03-16 19:49:07 +0800

    基本用法

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ import numpy as np
 
 print(paddle.__version__)
 
+#%%
+
+a = paddle.randn([4, 2])
+b = paddle.arange(1, 3, dtype='float32')
+
+print(a)
+print(b)
+
+c = a + b
+print(c)
+
+d = paddle.matmul(a, b)
+print(d)
+
