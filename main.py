f7e6031b0b16aaba64b43d88345ef595b6c0f600
songhaibei <songhaibei7305@163.com>
2022-03-16 17:57:38 +0800

    准备好运行飞桨

--- main.py
+++ main.py
@@ -20,3 +20,11 @@ y_data = paddle.to_tensor([[12.], [16.0], [20.0], [28.0], [30.0], [50.0]])
 
 linear = paddle.nn.Linear(in_features=1, out_features=1)
 
+#%%
+
+w_before_opt = linear.weight.numpy().item()
+b_before_opt = linear.bias.numpy().item()
+
+print("w before optimize: {}".format(w_before_opt))
+print("b before optimize: {}".format(b_before_opt))
+
