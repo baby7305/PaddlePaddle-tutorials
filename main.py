e5d710f4640fedddfa0097a4591dbc0b4c1c104f
songhaibei <songhaibei7305@163.com>
2022-03-17 17:47:30 +0800

    数据加载

--- main.py
+++ main.py
@@ -3,3 +3,16 @@
 import paddle
 print(paddle.__version__)
 
+#%%
+
+from paddle.vision.transforms import Compose, Normalize
+
+transform = Compose([Normalize(mean=[127.5],
+                               std=[127.5],
+                               data_format='CHW')])
+# 使用transform对数据集做归一化
+print('download training data and load training data')
+train_dataset = paddle.vision.datasets.MNIST(mode='train', transform=transform)
+test_dataset = paddle.vision.datasets.MNIST(mode='test', transform=transform)
+print('load finished')
+
