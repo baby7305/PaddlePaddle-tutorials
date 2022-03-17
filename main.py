94ff9dfa3ba9854776806241dcefcd6ec433f7d1
songhaibei <songhaibei7305@163.com>
2022-03-17 17:48:12 +0800

    数据加载

--- main.py
+++ main.py
@@ -16,3 +16,13 @@ train_dataset = paddle.vision.datasets.MNIST(mode='train', transform=transform)
 test_dataset = paddle.vision.datasets.MNIST(mode='test', transform=transform)
 print('load finished')
 
+#%%
+
+import numpy as np
+import matplotlib.pyplot as plt
+train_data0, train_label_0 = train_dataset[0][0],train_dataset[0][1]
+train_data0 = train_data0.reshape([28,28])
+plt.figure(figsize=(2,2))
+plt.imshow(train_data0, cmap=plt.cm.binary)
+print('train_data0 label is: ' + str(train_label_0))
+
