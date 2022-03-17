4fafcc143e59cbb1f7cd94426afad54d46fbe448
songhaibei <songhaibei7305@163.com>
2022-03-17 16:31:15 +0800

    数据处理

--- main.py
+++ main.py
@@ -17,3 +17,13 @@ print(paddle.__version__)
 #下载数据
 !wget https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data -O housing.data 
 
+#%%
+
+# 从文件导入数据
+datafile = './housing.data'
+housing_data = np.fromfile(datafile, sep=' ')
+feature_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE','DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+feature_num = len(feature_names)
+# 将原始数据进行Reshape，变成[N, 14]这样的形状
+housing_data = housing_data.reshape([housing_data.shape[0] // feature_num, feature_num])
+
