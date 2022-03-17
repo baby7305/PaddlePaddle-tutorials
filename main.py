e49249ef67ad839201ba7d286daccc707108098a
songhaibei <songhaibei7305@163.com>
2022-03-17 16:10:15 +0800

    环境配置

---
+++ main.py
@@ -0,0 +1,14 @@
+#%%
+
+import paddle
+import numpy as np
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore")
+
+print(paddle.__version__)
+
