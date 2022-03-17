a1b6b634c73b0fecf376fe6c3a7035166febf213
songhaibei <songhaibei7305@163.com>
2022-03-17 08:56:42 +0800

    模型组建

--- main.py
+++ main.py
@@ -16,3 +16,31 @@ print(paddle.__version__)
 train_dataset = MNIST(mode='train', transform=ToTensor())
 test_dataset = MNIST(mode='test', transform=ToTensor())
 
+#%%
+
+class MyModel(Layer):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.conv1 = paddle.nn.Conv2D(in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=2)
+        self.max_pool1 = MaxPool2D(kernel_size=2, stride=2)
+        self.conv2 = Conv2D(in_channels=6, out_channels=16, kernel_size=5, stride=1)
+        self.max_pool2 = MaxPool2D(kernel_size=2, stride=2)
+        self.linear1 = Linear(in_features=16*5*5, out_features=120)
+        self.linear2 = Linear(in_features=120, out_features=84)
+        self.linear3 = Linear(in_features=84, out_features=10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.max_pool1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = self.max_pool2(x)
+        x = paddle.flatten(x, start_axis=1, stop_axis=-1)
+        x = self.linear1(x)
+        x = F.relu(x)
+        x = self.linear2(x)
+        x = F.relu(x)
+        x = self.linear3(x)
+        return x
+
