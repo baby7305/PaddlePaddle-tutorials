be83167c656b3c905ae0eadf2a1b646ef73efdec
songhaibei <songhaibei7305@163.com>
2022-03-18 14:24:03 +0800

    环境设置

---
+++ main.py
@@ -0,0 +1,13 @@
+#%%
+
+import os
+import io
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image as PilImage
+
+import paddle
+from paddle.nn import functional as F
+
+paddle.__version__
+
