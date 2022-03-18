04dcc30d5ee797a794a24474b88322c4029451fa
songhaibei <songhaibei7305@163.com>
2022-03-18 17:23:29 +0800

    数据集
    PetDataSet数据集抽样展示

--- main.py
+++ main.py
@@ -62,3 +62,31 @@ write_file('train', images[:-eval_num], labels[:-eval_num])
 write_file('test', images[-eval_num:], labels[-eval_num:])
 write_file('predict', images[-eval_num:], labels[-eval_num:])
 
+#%%
+
+with open('./train.txt', 'r') as f:
+    i = 0
+
+    for line in f.readlines():
+        image_path, label_path = line.strip().split('\t')
+        image = np.array(PilImage.open(image_path))
+        label = np.array(PilImage.open(label_path))
+    
+        if i > 2:
+            break
+        # 进行图片的展示
+        plt.figure()
+
+        plt.subplot(1,2,1), 
+        plt.title('Train Image')
+        plt.imshow(image.astype('uint8'))
+        plt.axis('off')
+
+        plt.subplot(1,2,2), 
+        plt.title('Label')
+        plt.imshow(label.astype('uint8'), cmap='gray')
+        plt.axis('off')
+
+        plt.show()
+        i = i + 1
+
