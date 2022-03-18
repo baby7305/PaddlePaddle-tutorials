561e7b09bc45e2ae1d731ee3bcdf263b85c5805a
songhaibei <songhaibei7305@163.com>
2022-03-18 17:34:44 +0800

    模型组网
    定义Encoder编码器

--- main.py
+++ main.py
@@ -249,3 +249,45 @@ class SeparableConv2D(paddle.nn.Layer):
 
         return out
 
+#%%
+
+class Encoder(paddle.nn.Layer):
+    def __init__(self, in_channels, out_channels):
+        super(Encoder, self).__init__()
+        
+        self.relus = paddle.nn.LayerList(
+            [paddle.nn.ReLU() for i in range(2)])
+        self.separable_conv_01 = SeparableConv2D(in_channels, 
+                                                 out_channels, 
+                                                 kernel_size=3, 
+                                                 padding='same')
+        self.bns = paddle.nn.LayerList(
+            [paddle.nn.BatchNorm2D(out_channels) for i in range(2)])
+        
+        self.separable_conv_02 = SeparableConv2D(out_channels, 
+                                                 out_channels, 
+                                                 kernel_size=3, 
+                                                 padding='same')
+        self.pool = paddle.nn.MaxPool2D(kernel_size=3, stride=2, padding=1)
+        self.residual_conv = paddle.nn.Conv2D(in_channels, 
+                                              out_channels, 
+                                              kernel_size=1, 
+                                              stride=2, 
+                                              padding='same')
+
+    def forward(self, inputs):
+        previous_block_activation = inputs
+        
+        y = self.relus[0](inputs)
+        y = self.separable_conv_01(y)
+        y = self.bns[0](y)
+        y = self.relus[1](y)
+        y = self.separable_conv_02(y)
+        y = self.bns[1](y)
+        y = self.pool(y)
+        
+        residual = self.residual_conv(previous_block_activation)
+        y = paddle.add(y, residual)
+
+        return y
+
