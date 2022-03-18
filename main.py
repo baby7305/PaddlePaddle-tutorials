5720eb5d3e2918d8fcd6052c5b428ccece35e7e2
songhaibei <songhaibei7305@163.com>
2022-03-18 17:00:45 +0800

    数据集
    数据集下载

--- main.py
+++ main.py
@@ -11,3 +11,10 @@ from paddle.nn import functional as F
 
 paddle.__version__
 
+#%%
+
+!wget http://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz
+!wget http://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz
+!tar -xf images.tar.gz
+!tar -xf annotations.tar.gz
+
