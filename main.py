72fcf17605e34547c21cee605bb868cf62c323dd
songhaibei <songhaibei7305@163.com>
2022-03-18 11:35:54 +0800

    数据加载
    数据集介绍

--- main.py
+++ main.py
@@ -10,3 +10,24 @@ from collections import defaultdict
 
 print(paddle.__version__)
 
+#%%
+
+import paddle.vision.transforms as T
+transform = T.Compose([T.Transpose((2, 0, 1))])
+
+cifar10_train = paddle.vision.datasets.Cifar10(mode='train', transform=transform)
+x_train = np.zeros((50000, 3, 32, 32))
+y_train = np.zeros((50000, 1), dtype='int32')
+
+for i in range(len(cifar10_train)):
+    train_image, train_label = cifar10_train[i]
+    
+    # normalize the data
+    x_train[i,:, :, :] = train_image / 255.
+    y_train[i, 0] = train_label
+
+y_train = np.squeeze(y_train)
+
+print(x_train.shape)
+print(y_train.shape)
+
