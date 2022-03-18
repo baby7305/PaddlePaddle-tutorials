277d8eac27bc7f0b592597304874ae5cb50f56bb
songhaibei <songhaibei7305@163.com>
2022-03-18 19:24:11 +0800

    模型组网
    定义Decoder编码器

--- main.py
+++ main.py
@@ -291,3 +291,48 @@ class Encoder(paddle.nn.Layer):
 
         return y
 
+#%%
+
+class Decoder(paddle.nn.Layer):
+    def __init__(self, in_channels, out_channels):
+        super(Decoder, self).__init__()
+
+        self.relus = paddle.nn.LayerList(
+            [paddle.nn.ReLU() for i in range(2)])
+        self.conv_transpose_01 = paddle.nn.Conv2DTranspose(in_channels, 
+                                                           out_channels, 
+                                                           kernel_size=3, 
+                                                           padding=1)
+        self.conv_transpose_02 = paddle.nn.Conv2DTranspose(out_channels, 
+                                                           out_channels, 
+                                                           kernel_size=3, 
+                                                           padding=1)
+        self.bns = paddle.nn.LayerList(
+            [paddle.nn.BatchNorm2D(out_channels) for i in range(2)]
+        )
+        self.upsamples = paddle.nn.LayerList(
+            [paddle.nn.Upsample(scale_factor=2.0) for i in range(2)]
+        )
+        self.residual_conv = paddle.nn.Conv2D(in_channels, 
+                                              out_channels, 
+                                              kernel_size=1, 
+                                              padding='same')
+
+    def forward(self, inputs):
+        previous_block_activation = inputs
+
+        y = self.relus[0](inputs)
+        y = self.conv_transpose_01(y)
+        y = self.bns[0](y)
+        y = self.relus[1](y)
+        y = self.conv_transpose_02(y)
+        y = self.bns[1](y)
+        y = self.upsamples[0](y)
+        
+        residual = self.upsamples[1](previous_block_activation)
+        residual = self.residual_conv(residual)
+        
+        y = paddle.add(y, residual)
+        
+        return y
+
