be4a1de76df863320b9375ae9bf9f31a5c53f09e
songhaibei <songhaibei7305@163.com>
2022-03-18 10:23:43 +0800

    环境配置

---
+++ main.py
@@ -0,0 +1,10 @@
+#%%
+
+import paddle
+import paddle.nn.functional as F
+from paddle.vision.transforms import ToTensor
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(paddle.__version__)
+
