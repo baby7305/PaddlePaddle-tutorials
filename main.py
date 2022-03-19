fd45bace6112dd0d5557fa5658681d0a2d7c1f06
songhaibei <songhaibei7305@163.com>
2022-03-19 13:56:03 +0800

    预测前准备
    像定义训练Reader一样定义预测Reader

--- main.py
+++ main.py
@@ -201,3 +201,41 @@ model.fit(train_data=Reader(DATA_PATH),
             verbose=1,
             drop_last=True)
 
+#%%
+
+# 与训练近似，但不包含Label
+class InferReader(Dataset):
+    def __init__(self, dir_path=None, img_path=None):
+        """
+        数据读取Reader(预测)
+        :param dir_path: 预测对应文件夹（二选一）
+        :param img_path: 预测单张图片（二选一）
+        """
+        super().__init__()
+        if dir_path:
+            # 获取文件夹中所有图片路径
+            self.img_names = [i for i in os.listdir(dir_path) if os.path.splitext(i)[1] == ".jpg"]
+            self.img_paths = [os.path.join(dir_path, i) for i in self.img_names]
+        elif img_path:
+            self.img_names = [os.path.split(img_path)[1]]
+            self.img_paths = [img_path]
+        else:
+            raise Exception("请指定需要预测的文件夹或对应图片路径")
+
+    def get_names(self):
+        """
+        获取预测文件名顺序 
+        """
+        return self.img_names
+
+    def __getitem__(self, index):
+        # 获取图像路径
+        file_path = self.img_paths[index]
+        # 使用Pillow来读取图像数据并转成Numpy格式
+        img = Image.open(file_path)
+        img = np.array(img, dtype="float32").reshape((IMAGE_SHAPE_C, IMAGE_SHAPE_H, IMAGE_SHAPE_W)) / 255
+        return img
+
+    def __len__(self):
+        return len(self.img_paths)
+
