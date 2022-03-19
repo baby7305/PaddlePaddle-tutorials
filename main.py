a277f7dbc0b11c49cea42ce1fda75d68ab0b1073
songhaibei <songhaibei7305@163.com>
2022-03-19 14:15:20 +0800

    开始预测

--- main.py
+++ main.py
@@ -263,3 +263,40 @@ for img_id, img_name in enumerate(os.listdir(INFER_DATA_PATH)):
     plt.xlabel("Img name: " + img_name)
 plt.show()
 
+#%%
+
+# 编写简易版解码器
+def ctc_decode(text, blank=10):
+    """
+    简易CTC解码器
+    :param text: 待解码数据
+    :param blank: 分隔符索引值
+    :return: 解码后数据
+    """
+    result = []
+    cache_idx = -1
+    for char in text:
+        if char != blank and char != cache_idx:
+            result.append(char)
+        cache_idx = char
+    return result
+
+
+# 实例化推理模型
+model = paddle.Model(Net(is_infer=True), inputs=input_define)
+# 加载训练好的参数模型
+model.load(CHECKPOINT_PATH)
+# 设置运行环境
+model.prepare()
+
+# 加载预测Reader
+infer_reader = InferReader(INFER_DATA_PATH)
+img_names = infer_reader.get_names()
+results = model.predict(infer_reader, batch_size=BATCH_SIZE)
+index = 0
+for text_batch in results[0]:
+    for prob in text_batch:
+        out = ctc_decode(prob, blank=10)
+        print(f"文件名：{img_names[index]}，推理结果为：{out}")
+        index += 1
+
