1238ef8a656edfb1a2c9f4b45dbda741b50c1695
songhaibei <songhaibei7305@163.com>
2022-03-19 10:45:12 +0800

    自定义数据集读取器
    数据展示
    命令执行不动，直接复制链接到浏览器下载了文件

--- main.py
+++ main.py
@@ -3,3 +3,8 @@
 import paddle
 print(paddle.__version__)
 
+#%%
+
+# 下载数据集 
+!wget -O OCR_Dataset.zip https://bj.bcebos.com/v1/ai-studio-online/c91f50ef72de43b090298a38281e9c59a2d741eadd334f1cba7c710c5496e342?responseContentDisposition=attachment%3B%20filename%3DOCR_Dataset.zip&authorization=bce-auth-v1%2F0ef6765c1e494918bc0d4c3ca3e5c6d1%2F2020-10-27T09%3A50%3A21Z%2F-1%2F%2Fddc4aebed803af6c57dac46abba42d207961b78e7bc81744e8388395979b66fa
+
